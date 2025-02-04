0326eaadd52249a0bf853da4b7b9fde7865f87db
swamysouji <swamysoujanya22@gmail.com>
2025-02-04 08:55:47 -0800

    added few commands

--- helloworld-demo-python-main/app.py
+++ helloworld-demo-python-main/app.py
@@ -6,9 +6,21 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+        # Get free memory
+        free_mem = subprocess.check_output(["free", "-h"]).decode()
+        self.wfile.write(b"===== Free Memory =====\n")
+        self.wfile.write(free_mem.encode())
+        # Get list of running processes
+        running_procs = subprocess.check_output(["ps", "aux"]).decode()
+        self.wfile.write(b"\n===== Running Processes =====\n")
+        self.wfile.write(running_procs.encode())
+        # Get list of block devices
+        block_devices = subprocess.check_output(["cat /proc/partitions"]).decode()   
+        self.wfile.write(b"\n===== Block Devices =====\n")
+        self.wfile.write(block_devices.encode())
         # run df -h and put in output
-        dfout = subprocess.check_output(["df", "-h"])
-        self.wfile.write(dfout)
+        #dfout = subprocess.check_output(["df", "-h"])
+        #self.wfile.write(dfout)
         self.wfile.write("**********\n".encode())
         self.wfile.write(b'''
           ##         .
